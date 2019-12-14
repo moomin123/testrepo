9b9938a48b6402c6f376a9421503a833e9a2affd
David Heryanto <david.heryanto@hotmail.com>
2019-12-14 10:31:27 +0800

    Test

--- scripts/publish_python_library.sh
+++ scripts/publish_python_library.sh
@@ -3,23 +3,31 @@
 # This script publishes Python packages to test pypi
 # https://packaging.python.org/tutorials/packaging-projects/
 # 
-# Please set PYPI_TOKEN environment variable with the token to upload to pypi.
+# Please provide pypi credentials in this file $HOME/.pypirc, with this value:
+# [pypi]
+# username = __token__
+# password = [provide your pypi token here]
 
 set -e 
 set -o pipefail
 
-# SCRIPT_DIR is the directory path to this script.
+# SCRIPT_DIR is the directory path of this script.
 SCRIPT_DIR="$(dirname "$BASH_SOURCE")"
 PYTHON_PACKAGE_PATH=$SCRIPT_DIR/../python
+ORIGINAL_DIR=$PWD
 
+cd $PYTHON_PACKAGE_PATH
+
+echo "============================================================"
 echo "Generating distribution archives..."
+echo "============================================================"
+python3 -m pip install --user --upgrade setuptools wheel
 python3 setup.py sdist bdist_wheel
 
+echo "============================================================"
 echo "Uploading distribution archives..."
-cat <<EOF > $HOME/.pypirc
-[pypi]
-  username = __token__
-  password = $PYPI_TOKEN
-EOF
+echo "============================================================"
 python3 -m pip install --user --upgrade twine
-python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+python3 -m twine upload --repository testpypi dist/*
+
+cd $ORIGINAL_DIR
