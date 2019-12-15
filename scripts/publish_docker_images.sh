5ef958245ad2c4e2299d657b3261f1f6042a9dec
David Heryanto <david.heryanto@hotmail.com>
2019-12-15 13:58:39 +0800

    Update docker publish script

--- scripts/publish_docker_images.sh
+++ scripts/publish_docker_images.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
 gcloud -q auth configure-docker
+TAG=${PULL_BASE_REF:1}
+
+docker build -t gcr.io/kf-feast/test:$TAG -t gcr.io/kf-feast/test:latest .
+docker push gcr.io/kf-feast/test:$TAG
+docker push gcr.io/kf-feast/test:latest
